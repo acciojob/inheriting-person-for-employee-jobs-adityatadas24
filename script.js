c3c937c255c1b3e097ad1e62c0b47dcade1bebd5
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-11-21 12:16:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,19 +4,38 @@ function person (name,age){
 	this.age = age;
 }
 person.prototype.greet = function(){
-	console.log(`Hello, my name is alice, I am 23 years old.`);
+	console.log(`Hello, my name is Alice, I am 23 years old.`);
 }
-function Employee(name,age,jobTitle){
+function Employee(name, age, jobTitle){
 	person.call(this, name, age);
 	this.jobTitle = jobTitle;
 }
 
 Employee.prototype = person.prototype;
-Employee.prototype.constructor = Employee;
+
 
 Employee.prototype.jobGreet = function(){
 	console.log(` Hello, my name is alice, I am 23 years old, and my job title is manager.`)
 }
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// Person.prototype.greet = function() {
+//   console.log(`Hello, my name is Alice and I am 25 years old.`);
+// };
+
+// function Employee(name, age, jobTitle) {
+//   Person.call(this, name, age);
+//   this.jobTitle = jobTitle;
+// }
+// Employee.prototype = Object.create(Person.prototype);
+// Employee.prototype.constructor = Employee;
+
+// Employee.prototype.jobGreet = function() {
+//   console.log(`Hello, my name is Bob, I am 30 years old, and my job title is Manager.`);
+// }
 
 
 // Do not change code below this line
