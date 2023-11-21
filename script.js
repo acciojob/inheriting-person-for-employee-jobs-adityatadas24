77ec2e841b4a459a77888ec022faea4799cf5b27
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-11-21 11:57:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,36 +4,21 @@ function Person(name, age) {
   this.age = age;
 }
 
-// Add a method to the Person prototype
-Person.prototype.greet = function () {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
-// Employee constructor
 function Employee(name, age, jobTitle) {
-  // Call the parent constructor (Person) with the provided arguments
   Person.call(this, name, age);
   this.jobTitle = jobTitle;
 }
 
-// Inherit the Person prototype in the Employee prototype
-Employee.prototype = Person.prototype;
-
-// Set the constructor of the Employee prototype to Employee
+Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
-// Add a method to the Employee prototype
-Employee.prototype.jobGreet = function () {
-  console.log(
-    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
-  );
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
-const person1 = new Person("Alice", 25);
-person1.greet(); 
-
-const employee1 = new Employee("Bob", 30, "Software Engineer");
-employee1.greet();
-employee1.jobGreet(); 
 
 // Do not change code below this line
 window.Person = Person;
